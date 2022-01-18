5dd054eb9d30c1bfbdeee3cfa5c2ebcea96b062b
Danil Kabirov <gewery@mail.ru>
2022-01-18 22:50:25 +0300

    Added implementation of client-server

---
+++ client.py
@@ -0,0 +1,63 @@
+import socket
+import os
+
+
+def send_string(st):
+    encoded_command = bytes(st, 'utf-8')
+    kb = bytearray()
+    for i in range(1024 - len(encoded_command)):
+        kb.append(0)
+    kb += encoded_command
+    sock.send(kb)
+
+
+def receive_string():
+    st = bytearray(sock.recv(1024))
+    while len(st) != 0 and st[0] == 0:
+        st.remove(0)
+
+    return st.decode('utf-8')
+
+
+def download_file(server_location):
+    send_string(server_location)
+    response = receive_string()
+    if response != 'ACK':
+        print(response)
+        return
+
+    client_location = server_location
+    if os.path.exists(client_location):
+        print('File already exists, saving it as: ', end='')
+        client_location, ext = client_location[:client_location.rfind('.')], client_location[
+                                                                             client_location.rfind('.') + 1:]
+
+        number = 1
+        while os.path.exists(client_location + '_' + str(number) + '.' + ext):
+            number += 1
+
+        client_location += '_' + str(number) + '.' + ext
+
+        print(client_location)
+
+    f = open(client_location, 'wb+')
+    number_of_kb = int(receive_string())
+
+    for i in range(number_of_kb):
+        f.write(sock.recv(1024))
+
+    f.close()
+
+    print('file ' + client_location + ' received')
+
+
+port = 8800
+
+address = input('Enter address to connect: ')
+
+sock = socket.socket()
+sock.connect((address, port))
+
+while True:
+    server_location = input('Enter the file location: ')
+    download_file(server_location)
---
+++ server.py
@@ -0,0 +1,63 @@
+import socket
+import os
+
+
+def send_file(file_location):
+    f = open(file_location, 'rb')
+    to_send = []
+    chunk = f.read(1024)
+    while chunk:
+        to_send.append(chunk)
+        chunk = f.read(1024)
+
+    send_string(str(len(to_send)))
+
+    for kb in to_send:
+        con.send(kb)
+
+    f.close()
+
+    print('file ' + file_location + ' sent')
+
+
+def receive_string():
+    st = bytearray(con.recv(1024))
+    while len(st) != 0 and st[0] == 0:
+        st.remove(0)
+
+    return st.decode('utf-8')
+
+
+def send_string(str):
+    print('sending string to client:', str)
+    encoded_command = bytes(str, 'utf-8')
+    kb = bytearray()
+    for i in range(1024 - len(encoded_command)):
+        kb.append(0)
+    kb += encoded_command
+    con.send(kb)  # send command with 0-bytes in the beginning)
+
+
+port = 8800
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.bind(('', port))
+s.listen()
+
+con, addr = s.accept()
+print(str(addr) + ' connected')
+
+while True:
+    file_location = receive_string()
+    print('Requested file: ', file_location)
+
+    if not os.path.exists(file_location):
+        error = 'File ' + file_location + ' does not exists'
+        send_string('Error: ' + error)
+    else:
+        send_string('ACK')
+        send_file(file_location)
+
+
+
